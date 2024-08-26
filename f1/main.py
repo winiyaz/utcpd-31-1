670d0dae9bf9f0a2f5d41fcaf77e242120f978ca
winiyaz <winiyaz@proton.me>
2024-08-25 20:00:13 -0700

    ✔️FlashGame - chgPaths

--- f1/main.py
+++ f1/main.py
@@ -1,5 +1,22 @@
 # Solution for the project
 
-BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
+
+BOOTY_COLOR = "#400036"
+
+# --------------------- Window Setup --------------------- #
+window = Tk()
+window.title("AssAndPussy")
+window.config(padx=50, pady=50, bg=BOOTY_COLOR)
+
+# --------------------- CanvasObjet --------------------- #
+
+canvas = Canvas(width=800, height=526)
+cf_img = PhotoImage(file="")
+
+
+
+# --------------------- Window Start Loop --------------------- #
+window.mainloop()
 
 
